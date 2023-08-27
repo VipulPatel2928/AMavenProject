5b941b40b59453c70029e8af4f658825ec53bbf6
hvpswtester <hvpswtester@gmail.com>
2023-08-27 11:30:49 +0530

    27082023 if for while do while

--- src/test/java/IfForWhileDowhile/WhileDoWhile.java
+++ src/test/java/IfForWhileDowhile/WhileDoWhile.java
@@ -1,3 +1,4 @@
+package IfForWhileDowhile;
 
 public class WhileDoWhile {
 
@@ -6,23 +7,23 @@ public class WhileDoWhile {
 		String str = "java1";
 		int i = 1;
 		while (str.equals("java")) {
-			if(i>10) {
+			if (i > 10) {
 				break;
 			}
 			i++;
 			System.out.println("Condition Matched");
-		}//end of while
+		} // end of while
 		System.out.println("<--------------------------------------------------------------->");
-		
+
 		i = 1;
-	  do {
-		  if(i>10) {
+		do {
+			if (i > 10) {
 				break;
 			}
 			i++;
 			System.out.println("Condition Matched");
-	} while (str.equals("java"));
-	  System.out.println("<--------------------------------------------------------------->");
-		
+		} while (str.equals("java"));
+		System.out.println("<--------------------------------------------------------------->");
+
 	}
 }
