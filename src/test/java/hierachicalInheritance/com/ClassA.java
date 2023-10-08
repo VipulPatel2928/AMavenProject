00575cdc083296aeee5ce459f6f89ab487f55ee6
hvpswtester <hvpswtester@gmail.com>
2023-10-08 11:34:53 +0530

    Up to date 08102023

---
+++ src/test/java/hierachicalInheritance/com/ClassA.java
@@ -0,0 +1,10 @@
+package hierachicalInheritance.com;
+
+public class ClassA {
+	public void classAMethod() {
+		System.out.println("classAMethod");
+	}
+	public static void main(String[] args) {
+	
+	}
+}
